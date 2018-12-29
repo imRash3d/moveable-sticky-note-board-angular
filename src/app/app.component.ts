a2b09db5667abecd491608ae5b9eb41b4b9d6837
rashed <rashed.3rd@gmail.com>
2018-12-29 20:42:55 +0600

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular-boilarplate';
+  title = "Sticky Notes";
+  issues = [{ id: 1, text: " Create User functionality ", name: "Jhon Doe" }];
+  progress = [{ id: 1, text: "Create Dashboard Functionality", name: "Smith" }];
+  completed = [{ id: 4, text: "Add responsive design", name: "Dev" }];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+    console.log(this.completed);
+  }
+
+  addNotes(form) {
+    const data = {
+      id: this.issues.length + 1,
+      text: form.note,
+      name: form.name
+    };
+    this.issues.push(data);
+  }
+
+  deleteNote(id, type) {
+    console.log(id,type)
+    if (type === "issues") {
+      this.issues = this.issues.filter(item => item.id != id);
+    } else if (type === "progress") {
+      this.progress = this.progress.filter(item => item.id != id);
+    } else {
+      this.completed = this.completed.filter(item => item.id != id);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { AllNotesComponent } from "./all-notes/all-notes.component";
+import { AddNotesComponent } from "./add-notes/add-notes.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AllNotesComponent, AddNotesComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, DragDropModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
